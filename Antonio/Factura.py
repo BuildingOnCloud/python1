15fa682c80565c6e7ba4d594ca787a0bbb602212
Antonio Hernandez <antonio.javier1977@gmail.com>
2023-09-17 23:59:51 -0400

    archivo de Facturacion

---
+++ Antonio/Factura.py
@@ -0,0 +1,20 @@
+print("Factura de Compra")
+print("------------------")
+print("Nombre del producto:")
+producto = input()
+print("cantidad de", producto)
+p_cantidad = int(input())
+print("Valor unitario de", producto)
+p_unitario = float(input())
+
+p_st = p_cantidad * p_unitario
+
+subTotal = p_st
+IVA = subTotal *0.15
+Total = subTotal + IVA
+
+print ("--------------------")
+
+print ("    Sub-Total:" ,p_st)
+print ("          IVA:" ,IVA)
+print ("Total a pagar:" ,Total)
